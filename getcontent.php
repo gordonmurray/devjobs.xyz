ad9c8a08442cf520a650fefbc47e5a851204dbd2
Gordon <gordon@murrion.com>
2015-10-25 22:03:57 +0000

    Added a simple cache to avoid calling Reddit on each load

--- getcontent.php
+++ getcontent.php
@@ -1,62 +1,76 @@
 <?php
 
-$sources = array(
-    'https://www.reddit.com/r/forhire/new.json',
-    'https://www.reddit.com/r/hiring/new.json',
-);
+$cacheFile = 'cache/filteredPosts.json';
 
-$posts = array();
-$filteredPosts = array();
+// first, check for a recent local copy
+if (file_exists($cacheFile) && (filemtime($cacheFile) < time() - 600)) {
 
-$keywordsPositive = array('php', 'laravel', 'slim', 'silex', 'aws', 'api', 'data');
-$keywordsNegative = array('seo');
-$keywordsStrip = array('[hiring]');
+    $filteredPostsJson = file_get_contents($cacheFile);
 
-foreach ($sources as $source) {
-    $contentJson = file_get_contents($source);
-    $contentArray = json_decode($contentJson, TRUE);
-    $posts = array_merge($posts, $contentArray['data']['children']);
-}
+} else {
 
-foreach ($posts as $post) {
+    $sources = array(
+        'https://www.reddit.com/r/forhire/new.json',
+        'https://www.reddit.com/r/hiring/new.json',
+    );
 
-    $score = 0;
-    $postTitle = $post['data']['title'];
-    $postHTMLText = html_entity_decode($post['data']['selftext_html']);
-    $postCreated = date("d/m/Y H:ia", $post['data']['created_utc']);
+    $posts = array();
+    $filteredPosts = array();
 
-    if (stristr($postTitle, 'Hiring') == TRUE) {
+    $keywordsPositive = array('php', 'laravel', 'slim', 'silex', 'aws', 'api', 'data');
+    $keywordsNegative = array('seo');
+    $keywordsStrip = array('[hiring]');
 
-        $postTitle = strtolower($postTitle);
-        $postHTMLText = strtolower($postHTMLText);
-
-        foreach ($keywordsPositive as $keyword) {
-            if (stristr($postTitle, $keyword) == true) {
-                $score++;
-            }
-            if (stristr($postHTMLText, $keyword) == true) {
-                $score++;
-            }
-        }
-
-        foreach ($keywordsNegative as $keyword) {
-            if (stristr($postTitle, $keyword) == true) {
-                $score--;
-            }
-            if (stristr($postHTMLText, $keyword) == true) {
-                $score--;
-            }
-        }
-
-        $postTitle = str_replace($keywordsStrip, '', $postTitle);
-
-        $post['data']['title'] = $postTitle;
-        $post['data']['text'] = strip_tags($postHTMLText);
-        $post['data']['created'] = $postCreated;
-        $post['data']['score'] = $score;
-
-        $filteredPosts[] = $post['data'];
+    foreach ($sources as $source) {
+        $contentJson = file_get_contents($source);
+        $contentArray = json_decode($contentJson, TRUE);
+        $posts = array_merge($posts, $contentArray['data']['children']);
     }
+
+    foreach ($posts as $post) {
+
+        $score = 0;
+        $postTitle = $post['data']['title'];
+        $postHTMLText = html_entity_decode($post['data']['selftext_html']);
+        $postCreated = date("d/m/Y H:ia", $post['data']['created_utc']);
+
+        if (stristr($postTitle, 'Hiring') == TRUE) {
+
+            $postTitle = strtolower($postTitle);
+            $postHTMLText = strtolower($postHTMLText);
+
+            foreach ($keywordsPositive as $keyword) {
+                if (stristr($postTitle, $keyword) == true) {
+                    $score++;
+                }
+                if (stristr($postHTMLText, $keyword) == true) {
+                    $score++;
+                }
+            }
+
+            foreach ($keywordsNegative as $keyword) {
+                if (stristr($postTitle, $keyword) == true) {
+                    $score--;
+                }
+                if (stristr($postHTMLText, $keyword) == true) {
+                    $score--;
+                }
+            }
+
+            $postTitle = str_replace($keywordsStrip, '', $postTitle);
+
+            $post['data']['title'] = $postTitle;
+            $post['data']['text'] = strip_tags($postHTMLText);
+            $post['data']['created'] = $postCreated;
+            $post['data']['score'] = $score;
+
+            $filteredPosts[] = $post['data'];
+        }
+    }
+
+    $filteredPostsJson = json_encode($filteredPosts);
+
+    file_put_contents($cacheFile, $filteredPostsJson);
 }
 
-echo json_encode($filteredPosts);
+echo $filteredPostsJson;
